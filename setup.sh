06fc5bac3bfc9de966549829b86a42c7459ee828
Ruaidhri-MacKenzie <31Coding09>
2023-02-02 13:47:11 +0000

    Added setup shell script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt install nodejs
+sudo apt install npm
+git clone https://github.com/Ruaidhri-MacKenzie/enterprise-web-lab-01.git
+cd enterprise-web-lab-01
+npm install
+node server.js&
+sudo ufw allow 8080/tcp
