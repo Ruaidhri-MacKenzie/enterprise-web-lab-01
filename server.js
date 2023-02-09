40f9a3922d565b9462bf0d5516332f3fe720f4bd
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2023-02-09 14:29:47 +0000

    work

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = require("./routes/router.js");
 const quoteRouter = require("./routes/quoteRouter.js");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const DBURI = process.env.DBURI || "mongodb://127.0.0.1:27017/quote";
 
 // Body parser - parse JSON in request body
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +18,9 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
 app.use("/calc", router);
 app.use("/quote", quoteRouter);
+
+// Connect to database
+mongoose.connect(DBURI);
 
 // Listen for requests
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
